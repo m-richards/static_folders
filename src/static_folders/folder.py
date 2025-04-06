13fefdc1bff0d58606b54ca4ffc1a4e30960f466
Matt Richards <mrichards7@outlook.com.au>
2025-04-06 20:37:52 +1000

    fmt

--- src/static_folders/folder.py
+++ src/static_folders/folder.py
@@ -14,7 +14,7 @@ U = TypeVar("U", bound="Folder")
 
 @define(slots=False)
 class Folder:
-    _raw_location: os.PathLike| str
+    _raw_location: os.PathLike | str
     location: Path = field(init=False)
 
     _reserved_attributes: ClassVar[Sequence[str]] = [
@@ -56,8 +56,10 @@ class Folder:
                 elif issubclass(annotation, Path):
                     provided_path: Path = getattr(self, attrib_name)
                     if not isinstance(provided_path, Path):
-                        msg = (f"Annotation for attribute {attrib_name} was Path, "
-                               f"but provided attribute was {provided_path!r}")
+                        msg = (
+                            f"Annotation for attribute {attrib_name} was Path, "
+                            f"but provided attribute was {provided_path!r}"
+                        )
                         raise TypeError(msg)
                     if provided_path.is_absolute():
                         msg = (
@@ -79,7 +81,7 @@ class Folder:
     @typing.overload
     def get_subfolder(self, name: str, subclass_class: Type[U] = ...) -> U: ...
 
-    def get_subfolder(self, name: str, *, subfolder_class: Type[U]| None = None) -> U| "Folder":
+    def get_subfolder(self, name: str, *, subfolder_class: Type[U] | None = None) -> U | "Folder":
         if subfolder_class is None:
             return Folder(self.location / name)
         else:
