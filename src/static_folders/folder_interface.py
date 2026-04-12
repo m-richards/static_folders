be4a0a6d3dfd9b514fc4cd85c75e2b7fa52f3a4c
Matt Richards <mrichards7@outlook.com.au>
2026-04-12 14:41:32 +1000

    .

--- src/static_folders/folder_interface.py
+++ src/static_folders/folder_interface.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import typing
 from abc import ABC, abstractmethod
+from typing import Generic
 
 from typing_extensions import Type, TypeVar
-from typing import Generic
 
 if typing.TYPE_CHECKING:
     from pathlib import Path
